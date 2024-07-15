1a67dcaeeaedc4af4685152ce64a6880b6e2f217
grepsuzette <grepsuzette@users.noreply.github.com>
2024-07-15 16:53:32 +0800

    almost ready to test against updated gnoweb (but untested still)

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.21
 toolchain go1.22.3
 
 require (
+	github.com/dietsche/rfsnotify v0.0.0-20200716145600-b37be6e4177f
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gnolang/gno v0.1.0-nightly.20240523.0.20240531193303-edb321f85bef
 	github.com/gorilla/mux v1.8.1
 	github.com/gotuna/gotuna v0.6.0
+	go.uber.org/zap v1.27.0
+	gopkg.in/fsnotify.v1 v1.4.7
 )
 
 require (
@@ -24,7 +27,6 @@ require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/dgraph-io/badger/v3 v3.2103.4 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
-	github.com/dietsche/rfsnotify v0.0.0-20200716145600-b37be6e4177f // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gnolang/cors v1.8.1 // indirect
 	github.com/gnolang/overflow v0.0.0-20170615021017-4d914c927216 // indirect
@@ -60,8 +62,9 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.25.0 // indirect
 	go.opentelemetry.io/otel/trace v1.25.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.1.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap/exp v0.2.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
 	golang.org/x/mod v0.16.0 // indirect
@@ -73,6 +76,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
 	google.golang.org/grpc v1.63.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
-	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,108 +1,85 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
+	"io/fs"
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"runtime"
 	"strings"
 	"syscall"
 	"time"
 
-	"github.com/dietsche/rfsnotify"
-	"github.com/gnolang/gno/tm2/pkg/amino"
-	abci "github.com/gnolang/gno/tm2/pkg/bft/abci/types"
-	"github.com/gnolang/gno/tm2/pkg/bft/rpc/client"
+	// ------------------------ copied over from cmd/gnoweb
+	"github.com/gnolang/gno/gno.land/pkg/gnoweb"
+	"github.com/gnolang/gno/gno.land/pkg/log"
 	osm "github.com/gnolang/gno/tm2/pkg/os"
-	"github.com/gnolang/gno/tm2/pkg/std"
-	"github.com/gorilla/mux"
-	"github.com/gotuna/gotuna"
+	"go.uber.org/zap/zapcore"
+	// ------------------------
+	"github.com/dietsche/rfsnotify"
 	"gopkg.in/fsnotify.v1"
 
-	"github.com/gnolang/gno/gno.land/pkg/sdk/vm"       // for error types
-	"github.com/grepsuzette/gnAsteroid/static" // for static files
-	// "github.com/gnolang/gno/pkgs/sdk"       // for baseapp (info, status)
+	"github.com/gnolang/gno/gno.land/pkg/sdk/vm" // for error types
+	"github.com/grepsuzette/gnAsteroid/static"   // for static files
 )
-
-const (
-	qFileStr = "vm/qfile"
-)
-
-var flags struct {
-	asteroidDir  string
-	asteroidName string
-	bindAddr     string
-	remoteAddr   string
-	styleDir     string
-	captchaSite  string
-	faucetURL    string
-	helpChainID  string
-	helpRemote   string
-}
 
 var (
 	startedAt    time.Time
-	asteroidName string // read from cmdLine, or ./.TITLE, or is "CHANGEME"
+	asteroidName string // read from cmdLine, or file called .TITLE at root, or "CHANGEME"
+	asteroidDir  string
+	bindAddr     string
 )
 
 func init() {
-	flag.StringVar(&flags.asteroidDir, "asteroid-dir", "", "wiki directory location. [Mandatory!]")
-	flag.StringVar(&flags.asteroidName, "asteroid-name", "CHANGEME", "the asteroid name (website title). read from .TITLE, or CHANGEME")
-	flag.StringVar(&flags.bindAddr, "bind", "0.0.0.0:8888", "server listening address")
-	flag.StringVar(&flags.styleDir, "style-dir", "", "style directory (css, js, img). Default=<internal>")
-	flag.StringVar(&flags.remoteAddr, "remote", "127.0.0.1:26657", "remote gnoland node address")
-	flag.StringVar(&flags.captchaSite, "captcha-site", "", "recaptcha site key (if empty, captcha are disabled)")
-	flag.StringVar(&flags.faucetURL, "faucet-url", "http://localhost:5050", "faucet server URL")
-	flag.StringVar(&flags.helpChainID, "help-chainid", "dev", "help page's chainid")
-	flag.StringVar(&flags.helpRemote, "help-remote", "127.0.0.1:26657", "help page's remote addr")
 	startedAt = time.Now()
 }
 
-func makeApp() gotuna.App {
-	app := gotuna.App{
-		Static:    static.EmbeddedStatic,
-		ViewFiles: static.EmbeddedStatic, // prefix is "views/"
-		Router:    gotuna.NewMuxRouter(),
-	}
-	asteroidName = flags.asteroidName
-	if osm.FileExists(flags.asteroidDir + "/.TITLE") {
-		s := string(osm.MustReadFile(flags.asteroidDir + "/.TITLE"))
+func parseArgs(args []string) (gnoweb.Config, error) {
+	cfg := gnoweb.NewDefaultConfig()
+	fs := flag.NewFlagSet("gnoweb", flag.ContinueOnError)
+	fs.StringVar(&asteroidDir, "asteroid-dir", "", "wiki directory location. [Mandatory!]")
+	fs.StringVar(&asteroidName, "asteroid-name", "CHANGEME", "the asteroid name (website title). read from .TITLE, or CHANGEME")
+	fs.StringVar(&bindAddr, "bind", "0.0.0.0:8888", "server listening address")
+	fs.StringVar(&cfg.StyleDir, "style-dir", "", "style directory (css, js, img). Default is to use embedded gnoweb style")
+	fs.StringVar(&cfg.RemoteAddr, "remote", "127.0.0.1:26657", "remote gnoland node address")
+	fs.StringVar(&cfg.CaptchaSite, "captcha-site", "", "recaptcha site key (if empty, captcha are disabled)")
+	fs.StringVar(&cfg.FaucetURL, "faucet-url", "http://localhost:5050", "faucet server URL")
+	fs.StringVar(&cfg.HelpChainID, "help-chainid", "dev", "help page's chainid")
+	fs.StringVar(&cfg.HelpRemote, "help-remote", "127.0.0.1:26657", "help page's remote addr")
+	fs.StringVar(&cfg.WithAnalytics, "with-analytics", cfg.WithAnalytics, "enable privacy-first analytics")
+
+	// if asteroidName has default value, check if <asteroidDir>/.TITLE exists
+	if asteroidName == "CHANGEME" && osm.FileExists(asteroidDir+"/.TITLE") {
+		s := string(osm.MustReadFile(asteroidDir + "/.TITLE"))
 		asteroidName = strings.NewReplacer("<", "&lt;", ">", "&gt;").Replace(s)
 	}
-	app.Router.Handle("/", handlerHome(app))
-	app.Router.Handle("/faucet", handlerFaucet(app))
-	app.Router.Handle("/r/demo/boards:gnolang/6", handlerRedirect(app))
-	// NOTE: see rePathPart.
-	app.Router.Handle("/r/{rlmname:[a-z][a-z0-9_]*(?:/[a-z][a-z0-9_]*)+}/{filename:(?:.*\\.(?:gno|md|txt|mod)$)?}", handlerRealmFile(app))
-	app.Router.Handle("/r/{rlmname:[a-z][a-z0-9_]*(?:/[a-z][a-z0-9_]*)+}", handlerRealmMain(app))
-	app.Router.Handle("/r/{rlmname:[a-z][a-z0-9_]*(?:/[a-z][a-z0-9_]*)+}:{querystr:.*}", handlerRealmRender(app))
-	app.Router.Handle("/p/{filepath:.*}", handlerPackageFile(app))
-	fmt.Println("Echo " + flags.styleDir)
-	if osm.DirExists(flags.styleDir) {
-		fmt.Println("  custom style at " + flags.styleDir)
-		app.Router.Handle("/static/{path:(?:css|font|img)/.+}", handlerAsset(app))
-	}
-	// else the style is simply in embed.FS as well:
-	app.Router.Handle("/static/{path:.+}", handlerStaticFile(app))
-	app.Router.Handle("/favicon.ico", handlerFavicon(app))
-	app.Router.Handle("/status.json", handlerStatusJSON(app))
-	app.Router.Handle("/{anything:.*(?:\\.md)?}", handlerAnything(app))
-	return app
+
+	return cfg, fs.Parse(args)
 }
 
 func main() {
-	flag.Parse()
-	fmt.Printf("Serving %s on http://%s\n", flags.asteroidDir, flags.bindAddr)
+	cfg, e := parseArgs(os.Args[1:])
+	if e != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(1)
+	}
+	zapLogger := log.NewZapConsoleLogger(os.Stdout, zapcore.DebugLevel)
+	logger := log.ZapLoggerToSlog(zapLogger)
+	logger.Info("Serving", asteroidDir, " on ", "http://", bindAddr)
 
+	makeApp := func() http.Handler {
+		return gnoweb.MakeAppWithOptions(logger, cfg, gnoweb.Options{
+			RootHandler:     handlerHome,
+			NotFoundHandler: handlerAnything,
+		}).Router
+	}
+
+	none := map[string]string{}
 	server := &http.Server{
-		Addr:              flags.bindAddr,
+		Addr:              bindAddr,
 		ReadHeaderTimeout: 60 * time.Second,
-		Handler:           makeApp().Router,
+		Handler:           makeApp(),
 	}
 
 	// Detect SIGUSR1 -> invalidate all templates
@@ -114,7 +91,7 @@ func main() {
 			switch sig {
 			case syscall.SIGUSR1:
 				fmt.Println("received SIGUSR1.")
-				server.Handler = makeApp().Router
+				server.Handler = makeApp()
 			}
 		}
 	}()
@@ -130,27 +107,27 @@ func main() {
 						return
 					}
 					if event.Op&fsnotify.Write == fsnotify.Write {
-						fmt.Println("Reloading, modified: " + event.Name)
-						server.Handler = makeApp().Router
+						logger.Info("Reloading, modified: ", event.Name)
+						server.Handler = makeApp()
 					}
 				}
 			}
 		}()
 		watcher.AddRecursive(flags.asteroidDir)
-		if flags.styleDir != "" {
-			watcher.AddRecursive(flags.styleDir)
+		if flags.StyleDir != "" {
+			watcher.AddRecursive(flags.StyleDir)
 		}
 	}
 
 	if err := server.ListenAndServe(); err != nil {
-		fmt.Fprintf(os.Stderr, "HTTP server stopped with error: %+v\n", err)
+		logger.Error("HTTP server stopped with error: %+v\n", err)
 	}
+	return zapLogger.Sync()
 }
 
-func handlerHome(app gotuna.App) http.Handler {
+func handlerHome(_, app gotuna.App) http.Handler {
 	md := filepath.Join(flags.asteroidDir, "index.md")
 	homeContent := osm.MustReadFile(md)
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.NewTemplatingEngine().
 			Set("AsteroidName", asteroidName).
@@ -159,7 +136,7 @@ func handlerHome(app gotuna.App) http.Handler {
 	})
 }
 
-func handlerAnything(app gotuna.App) http.Handler {
+func handlerAnything(_, app gotuna.App) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		file := filepath.Join(flags.asteroidDir, mux.Vars(r)["anything"])
 		if osm.DirExists(file) && osm.FileExists(file+"/index.md") {
@@ -189,10 +166,10 @@ func handlerAnything(app gotuna.App) http.Handler {
 				Set("MainContent", string(content)).
 				Render(w, r, "views/generic.html", "views/funcs.html")
 		case strings.HasSuffix(file, ".jpg"),
-		     strings.HasSuffix(file, ".jpeg"),
-			 strings.HasSuffix(file, ".png"),
-			 strings.HasSuffix(file, ".gif"):
-				http.ServeFile(w, r, file)
+			strings.HasSuffix(file, ".jpeg"),
+			strings.HasSuffix(file, ".png"),
+			strings.HasSuffix(file, ".gif"):
+			http.ServeFile(w, r, file)
 		default:
 			if osm.FileExists(file) {
 				w.Write([]byte("Unrecognized extension"))
@@ -201,348 +178,4 @@ func handlerAnything(app gotuna.App) http.Handler {
 			}
 		}
 	})
-}
-
-func handlerStatusJSON(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var ret struct {
-			Gnoland struct {
-				Connected bool    `json:"connected"`
-				Error     *string `json:"error,omitempty"`
-				Height    *int64  `json:"height,omitempty"`
-				// processed txs
-				// active connections
-
-				Version *string `json:"version,omitempty"`
-				// Uptime    *float64 `json:"uptime-seconds,omitempty"`
-				// Goarch    *string  `json:"goarch,omitempty"`
-				// Goos      *string  `json:"goos,omitempty"`
-				// GoVersion *string  `json:"go-version,omitempty"`
-				// NumCPU    *int     `json:"num_cpu,omitempty"`
-			} `json:"gnoland"`
-			Website struct {
-				// Version string  `json:"version"`
-				Uptime    float64 `json:"uptime-seconds"`
-				Goarch    string  `json:"goarch"`
-				Goos      string  `json:"goos"`
-				GoVersion string  `json:"go-version"`
-				NumCPU    int     `json:"num_cpu"`
-			} `json:"website"`
-		}
-		ret.Website.Uptime = time.Since(startedAt).Seconds()
-		ret.Website.Goarch = runtime.GOARCH
-		ret.Website.Goos = runtime.GOOS
-		ret.Website.NumCPU = runtime.NumCPU()
-		ret.Website.GoVersion = runtime.Version()
-
-		ret.Gnoland.Connected = true
-		res, err := makeRequest(".app/version", []byte{})
-		if err != nil {
-			ret.Gnoland.Connected = false
-			errmsg := err.Error()
-			ret.Gnoland.Error = &errmsg
-		} else {
-			version := string(res.Value)
-			ret.Gnoland.Version = &version
-			ret.Gnoland.Height = &res.Height
-		}
-
-		out, _ := json.MarshalIndent(ret, "", "  ")
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(out)
-	})
-}
-
-// XXX temporary.
-func handlerRedirect(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/r/boards:gnolang/3", http.StatusFound)
-		app.NewTemplatingEngine().
-			Set("AsteroidName", asteroidName).
-			Render(w, r, "views/home.html", "views/funcs.html")
-	})
-}
-
-func handlerRealmMain(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		rlmname := vars["rlmname"]
-		rlmpath := "gno.land/r/" + rlmname
-		query := r.URL.Query()
-		if query.Has("help") {
-			// Render function helper.
-			funcName := query.Get("__func")
-			qpath := "vm/qfuncs"
-			data := []byte(rlmpath)
-			res, err := makeRequest(qpath, data)
-			if err != nil {
-				writeError(w, err)
-				return
-			}
-			var fsigs vm.FunctionSignatures
-			amino.MustUnmarshalJSON(res.Data, &fsigs)
-			// Fill fsigs with query parameters.
-			for i := range fsigs {
-				fsig := &(fsigs[i])
-				for j := range fsig.Params {
-					param := &(fsig.Params[j])
-					value := query.Get(param.Name)
-					param.Value = value
-				}
-			}
-			// Render template.
-			tmpl := app.NewTemplatingEngine()
-			tmpl.Set("AsteroidName", asteroidName)
-			tmpl.Set("FuncName", funcName)
-			tmpl.Set("RealmPath", rlmpath)
-			// tmpl.Set("FormBodyType", query.Get("body.type")) // when "textarea", <textarea> will be used instead of <input>
-			tmpl.Set("FormBodyType", "textarea") // when "textarea", <textarea> will be used instead of <input>
-			tmpl.Set("Remote", flags.helpRemote)
-			tmpl.Set("ChainID", flags.helpChainID)
-			tmpl.Set("DirPath", pathOf(rlmpath))
-			tmpl.Set("FunctionSignatures", fsigs)
-			tmpl.Render(w, r, "views/realm_help.html", "views/funcs.html")
-		} else {
-			// Ensure realm exists. TODO optimize.
-			qpath := qFileStr
-			data := []byte(rlmpath)
-			_, err := makeRequest(qpath, data)
-			if err != nil {
-				writeError(w, fmt.Errorf("error (%w) querying realm package)", err))
-				return
-			}
-			// Render blank query path, /r/REALM:.
-			handleRealmRender(app, w, r)
-		}
-	})
-}
-
-type pathLink struct {
-	URL  string
-	Text string
-}
-
-func handlerRealmRender(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handleRealmRender(app, w, r)
-	})
-}
-
-func handleRealmRender(app gotuna.App, w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	rlmname := vars["rlmname"]
-	rlmpath := "gno.land/r/" + rlmname
-	querystr := vars["querystr"]
-	if r.URL.Path == "/r/"+rlmname+":" {
-		// Redirect to /r/REALM if querypath is empty.
-		http.Redirect(w, r, "/r/"+rlmname, http.StatusFound)
-		return
-	}
-	qpath := "vm/qrender"
-	data := []byte(fmt.Sprintf("%s\n%s", rlmpath, querystr))
-	res, err := makeRequest(qpath, data)
-	if err != nil {
-		// XXX hack
-		if strings.Contains(err.Error(), "Render not declared") {
-			res = &abci.ResponseQuery{}
-			res.Data = []byte("realm package `" + rlmname + "` has no Render() function")
-		} else {
-			writeError(w, err)
-			return
-		}
-	}
-	// linkify querystr.
-	queryParts := strings.Split(querystr, "/")
-	pathLinks := []pathLink{}
-	for i, part := range queryParts {
-		pathLinks = append(pathLinks, pathLink{
-			URL:  "/r/" + rlmname + ":" + strings.Join(queryParts[:i+1], "/"),
-			Text: part,
-		})
-	}
-	// Render template.
-	tmpl := app.NewTemplatingEngine()
-	tmpl.Set("AsteroidName", asteroidName)
-	tmpl.Set("RealmName", rlmname)
-	tmpl.Set("RealmPath", rlmpath)
-	tmpl.Set("Query", querystr)
-	tmpl.Set("PathLinks", pathLinks)
-	tmpl.Set("Contents", string(res.Data))
-	tmpl.Render(w, r, "views/realm_render.html", "views/funcs.html")
-}
-
-func handlerRealmFile(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		diruri := "gno.land/r/" + vars["rlmname"]
-		filename := vars["filename"]
-		renderPackageFile(app, w, r, diruri, filename)
-	})
-}
-
-func handlerPackageFile(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		pkgpath := "gno.land/p/" + vars["filepath"]
-		diruri, filename := std.SplitFilepath(pkgpath)
-		if filename == "" && diruri == pkgpath {
-			// redirect to diruri + "/"
-			http.Redirect(w, r, "/p/"+vars["filepath"]+"/", http.StatusFound)
-			return
-		}
-		renderPackageFile(app, w, r, diruri, filename)
-	})
-}
-
-func renderPackageFile(app gotuna.App, w http.ResponseWriter, r *http.Request, diruri string, filename string) {
-	if filename == "" {
-		// Request is for a folder.
-		qpath := qFileStr
-		data := []byte(diruri)
-		res, err := makeRequest(qpath, data)
-		if err != nil {
-			writeError(w, err)
-			return
-		}
-		files := strings.Split(string(res.Data), "\n")
-		// Render template.
-		tmpl := app.NewTemplatingEngine()
-		tmpl.Set("AsteroidName", asteroidName)
-		tmpl.Set("DirURI", diruri)
-		tmpl.Set("DirPath", pathOf(diruri))
-		tmpl.Set("Files", files)
-		tmpl.Render(w, r, "views/package_dir.html", "views/funcs.html")
-	} else {
-		// Request is for a file.
-		filepath := diruri + "/" + filename
-		qpath := qFileStr
-		data := []byte(filepath)
-		res, err := makeRequest(qpath, data)
-		if err != nil {
-			writeError(w, err)
-			return
-		}
-		// Render template.
-		tmpl := app.NewTemplatingEngine()
-		tmpl.Set("AsteroidName", asteroidName)
-		tmpl.Set("DirURI", diruri)
-		tmpl.Set("DirPath", pathOf(diruri))
-		tmpl.Set("FileName", filename)
-		tmpl.Set("FileContents", string(res.Data))
-		tmpl.Render(w, r, "views/package_file.html", "views/funcs.html")
-	}
-}
-
-func makeRequest(qpath string, data []byte) (res *abci.ResponseQuery, err error) {
-	opts2 := client.ABCIQueryOptions{
-		// Height: height, XXX
-		// Prove: false, XXX
-	}
-	remote := flags.remoteAddr
-	cli, err := client.NewHTTPClient(remote)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create HTTP client, %w", err)
-	}
-	qres, err := cli.ABCIQueryWithOptions(
-		qpath, data, opts2)
-	if err != nil {
-		return nil, err
-	}
-	if qres.Response.Error != nil {
-		fmt.Printf("Log: %s\n",
-			qres.Response.Log)
-		return nil, qres.Response.Error
-	}
-	return &qres.Response, nil
-}
-
-func handlerAsset(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		s := flags.styleDir
-		file := filepath.Join(s, mux.Vars(r)["path"])
-		if !osm.FileExists(file) || osm.DirExists(file) {
-			http.Error(w, "Not Found", http.StatusNotFound)
-		} else {
-			// serve the file, based of its extension
-			http.ServeFile(w, r, file)
-		}
-	})
-}
-
-func handlerStaticFile(app gotuna.App) http.Handler {
-	fs := http.FS(app.Static)
-	fileapp := http.StripPrefix("/static", http.FileServer(fs))
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		fpath := filepath.Clean(vars["path"])
-		f, err := fs.Open(fpath)
-		if os.IsNotExist(err) {
-			handleNotFound(app, fpath, w, r)
-			return
-		}
-		stat, err := f.Stat()
-		if err != nil || stat.IsDir() {
-			handleNotFound(app, fpath, w, r)
-			return
-		}
-
-		// TODO: ModTime doesn't work for embed?
-		// w.Header().Set("ETag", fmt.Sprintf("%x", stat.ModTime().UnixNano()))
-		// w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%s", "31536000"))
-		fileapp.ServeHTTP(w, r)
-	})
-}
-
-func handlerFavicon(app gotuna.App) http.Handler {
-	if osm.DirExists(flags.styleDir) && osm.FileExists(flags.styleDir+"/img/favicon.ico") {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			http.ServeFile(w, r, flags.styleDir+"/img/favicon.ico")
-		})
-	} else {
-		fs := http.FS(app.Static)
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fpath := "img/favicon.ico"
-			f, err := fs.Open(fpath)
-			if os.IsNotExist(err) {
-				handleNotFound(app, fpath, w, r)
-				return
-			}
-			w.Header().Set("Content-Type", "image/x-icon")
-			w.Header().Set("Cache-Control", "public, max-age=604800") // 7d
-			io.Copy(w, f)
-		})
-	}
-}
-
-func handlerFaucet(app gotuna.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.NewTemplatingEngine().
-			Set("captchaSite", flags.captchaSite).
-			Set("faucetURL", flags.faucetURL).
-			Render(w, r, "views/faucet.html", "views/funcs.html")
-	})
-}
-
-func handleNotFound(app gotuna.App, path string, w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	app.NewTemplatingEngine().
-		Set("AsteroidName", asteroidName).
-		Set("title", "Not found").
-		Set("path", path).
-		Render(w, r, "views/404.html", "views/funcs.html")
-}
-
-func writeError(w http.ResponseWriter, err error) {
-	w.WriteHeader(500)
-	w.Write([]byte(err.Error()))
-}
-
-func pathOf(diruri string) string {
-	parts := strings.Split(diruri, "/")
-	if parts[0] == "gno.land" {
-		return "/" + strings.Join(parts[1:], "/")
-	} else {
-		panic(fmt.Sprintf("invalid dir-URI %q", diruri))
-	}
 }
