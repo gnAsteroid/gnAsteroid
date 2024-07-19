5aca5ef298581e531de921aa0c12d25ca9f4beed
grepsuzette <grepsuzette@users.noreply.github.com>
2024-07-19 20:16:03 +0800

    allow usage as a package (add HandleAsteroid())

--- main.go
+++ main.go
@@ -26,21 +26,104 @@ import (
 )
 
 var (
-	startedAt    time.Time
 	asteroidName string // read from cmdLine, or file called .TITLE at root, or "CHANGEME"
 	asteroidDir  string
 	styleDir     string
-	bindAddr     string
+	bindAddr     string // not used when HandleAsteroid()
 )
 
 //go:embed views/*.html
-var asteroidTemplates embed.FS // composed with gnoweb's, using merged_fs
+var newViews embed.FS // composed with gnoweb's, using merged_fs
 
 //go:embed default-style/*
 var defaultEmbedStyle embed.FS
 
-func init() {
-	startedAt = time.Now()
+// Run the whole thing (asteroid+gnoweb) as an http.Handler.
+// This function is exported so you can use this
+// as a lib (vercel?). In that case, you would not use
+// main, nor need args, or a server.
+//
+func HandleAsteroid(styleDir_, asteroidDir_, asteroidName_ string, cfg gnoweb.Config) http.Handler {
+	styleDir = styleDir_
+	asteroidDir = asteroidDir_
+	asteroidName = asteroidName_
+	bindAddr = "" // not used in that case
+	return makeApp(slog.Default(), cfg, StyleFsFrom(styleDir))
+}
+
+func makeApp(logger *slog.Logger, cfg gnoweb.Config, styleFs fs.FS) http.Handler {
+	gnowebViews, e := fs.Sub(gnoweb.DefaultViewsFiles(), "views")
+	if e != nil {
+		panic("Could not find gnoweb views: " + e.Error())
+	}
+	return gnoweb.MakeAppWithOptions(logger, cfg, gnoweb.Options{
+		RootHandler:     HandlerHome,
+		NotFoundHandler: HandlerAnything,
+		StyleFS:         styleFs,
+		ViewFS:          merged_fs.NewMergedFS(gnowebViews, newViews),
+	}).Router
+}
+
+// main() for cli gnAsteroid.
+// cli-less usage of this is HandleAsteroid().
+func main() {
+	zapLogger := log.NewZapConsoleLogger(os.Stdout, zapcore.DebugLevel)
+	logger := log.ZapLoggerToSlog(zapLogger)
+
+	cfg, e := parseArgs(os.Args[1:], logger)
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", e)
+		os.Exit(1)
+	}
+	logger.Info(fmt.Sprintf("Serving %s on http://%s", asteroidDir, bindAddr))
+	styleFs := StyleFsFrom(styleDir)
+	server := &http.Server{
+		Addr:              bindAddr,
+		ReadHeaderTimeout: 60 * time.Second,
+		Handler:           makeApp(logger, cfg, styleFs),
+	}
+
+	// SIGUSR1 -> invalidate all templates
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGUSR1)
+	go func() {
+		for {
+			sig := <-sigChan
+			switch sig {
+			case syscall.SIGUSR1:
+				fmt.Println("received SIGUSR1.")
+				server.Handler = makeApp(logger, cfg, styleFs)
+			}
+		}
+	}()
+
+	// Watch over asteroidDir for any change -> invalidate all templates
+	if watcher, err := rfsnotify.NewWatcher(); err == nil {
+		defer watcher.Close()
+		go func() {
+			for {
+				select {
+				case event, ok := <-watcher.Events:
+					if !ok {
+						return
+					}
+					if event.Op&fsnotify.Write == fsnotify.Write {
+						logger.Info("Reloading, modified: " + event.Name)
+						server.Handler = makeApp(logger, cfg, styleFs)
+					}
+				}
+			}
+		}()
+		watcher.AddRecursive(asteroidDir)
+		if styleDir != "" {
+			watcher.AddRecursive(styleDir)
+		}
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		logger.Error(fmt.Sprintf("HTTP server stopped with error: %+v\n", err))
+	}
+	zapLogger.Sync()
 }
 
 func parseArgs(args []string, logger *slog.Logger) (gnoweb.Config, error) {
@@ -58,12 +141,10 @@ func parseArgs(args []string, logger *slog.Logger) (gnoweb.Config, error) {
 	fs.StringVar(&cfg.HelpChainID, "help-chainid", "dev", "help page's chainid")
 	fs.StringVar(&cfg.HelpRemote, "help-remote", "127.0.0.1:26657", "help page's remote addr")
 	fs.BoolVar(&cfg.WithAnalytics, "with-analytics", cfg.WithAnalytics, "enable privacy-first analytics")
-
 	// let's parse cli, baby
 	if parseError := fs.Parse(args); parseError != nil {
 		return cfg, parseError
 	}
-
 	// if asteroidName has default value, check whether <asteroidDir>/.TITLE exists
 	if asteroidName == "CHANGEME" && osm.FileExists(asteroidDir+"/.TITLE") {
 		s := string(osm.MustReadFile(asteroidDir + "/.TITLE"))
@@ -71,95 +152,12 @@ func parseArgs(args []string, logger *slog.Logger) (gnoweb.Config, error) {
 		logger.Debug(fmt.Sprintf("asteroidName is %s and exists %s -> changing asteroidName to %q", asteroidName, asteroidDir+"/.TITLE", s))
 		asteroidName = s
 	}
-
 	return cfg, nil
 }
 
-func main() {
-	zapLogger := log.NewZapConsoleLogger(os.Stdout, zapcore.DebugLevel)
-	logger := log.ZapLoggerToSlog(zapLogger)
-
-	cfg, e := parseArgs(os.Args[1:], logger)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%+v\n", e)
-		os.Exit(1)
-	}
-	logger.Info(fmt.Sprintf("Serving %s on http://%s", asteroidDir, bindAddr))
-
-	var styleFs fs.FS
-	if styleDir == "" {
-		// embed style is newcomer-friendly
-		styleFs = defaultEmbedStyle
-	} else {
-		styleFs = os.DirFS(styleDir)
-	}
-
-	gnowebViews, e := fs.Sub(gnoweb.DefaultViewsFiles(), "views")
-	if e != nil {
-		panic("Could not find gnoweb views: " + e.Error())
-	}
-	makeApp := func() http.Handler {
-		return gnoweb.MakeAppWithOptions(logger, cfg, gnoweb.Options{
-			RootHandler:     handlerHome,
-			NotFoundHandler: handlerAnything,
-			StyleFS:         styleFs,
-			ViewFS: merged_fs.NewMergedFS(
-				gnowebViews,
-				asteroidTemplates,
-			),
-		}).Router
-	}
-
-	server := &http.Server{
-		Addr:              bindAddr,
-		ReadHeaderTimeout: 60 * time.Second,
-		Handler:           makeApp(),
-	}
-
-	// SIGUSR1 -> invalidate all templates
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGUSR1)
-	go func() {
-		for {
-			sig := <-sigChan
-			switch sig {
-			case syscall.SIGUSR1:
-				fmt.Println("received SIGUSR1.")
-				server.Handler = makeApp()
-			}
-		}
-	}()
-
-	// Watch over asteroidDir for any change -> invalidate all templates
-	if watcher, err := rfsnotify.NewWatcher(); err == nil {
-		defer watcher.Close()
-		go func() {
-			for {
-				select {
-				case event, ok := <-watcher.Events:
-					if !ok {
-						return
-					}
-					if event.Op&fsnotify.Write == fsnotify.Write {
-						logger.Info("Reloading, modified: " + event.Name)
-						server.Handler = makeApp()
-					}
-				}
-			}
-		}()
-		watcher.AddRecursive(asteroidDir)
-		if styleDir != "" {
-			watcher.AddRecursive(styleDir)
-		}
-	}
-
-	if err := server.ListenAndServe(); err != nil {
-		logger.Error(fmt.Sprintf("HTTP server stopped with error: %+v\n", err))
-	}
-	zapLogger.Sync()
-}
-
-func handlerHome(logger *slog.Logger, app gotuna.App, cfg *gnoweb.Config) http.Handler {
+// this is a handler that serves /
+// it is used when gnoweb needs to serve a root index.md file.
+func HandlerHome(logger *slog.Logger, app gotuna.App, cfg *gnoweb.Config) http.Handler {
 	index_md := filepath.Join(asteroidDir, "index.md")
 	homeContent := osm.MustReadFile(index_md)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -171,7 +169,10 @@ func handlerHome(logger *slog.Logger, app gotuna.App, cfg *gnoweb.Config) http.H
 	})
 }
 
-func handlerAnything(logger *slog.Logger, app gotuna.App, cfg *gnoweb.Config) http.Handler {
+// this is a fallthrough handler to
+// attempt serving markdown, images
+// in the structure.
+func HandlerAnything(logger *slog.Logger, app gotuna.App, cfg *gnoweb.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL.String()
 		url = html.UnescapeString(url)
@@ -218,4 +219,11 @@ func handlerAnything(logger *slog.Logger, app gotuna.App, cfg *gnoweb.Config) ht
 			}
 		}
 	})
+}
+
+func StyleFsFrom(styleDir string) fs.FS {
+	if styleDir == "" {
+		return defaultEmbedStyle
+	}
+	return os.DirFS(styleDir)
 }
